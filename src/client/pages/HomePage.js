40d11d03133f5cfef3e3c01fbb7088a6679c865f
Marcelo Honório dos Santos <marcelohonsa@gmail.com>
2023-06-11 09:08:31 -0400

    fix

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <div>I will do the best work!! And I WILL WINNNNNNN!!!! BELIEVE!!</div>
-            <button onClick={() => {console.log('Im Here!!!')}}>Press me!</button>
+        <div className='center-align' style={{marginTop: '200px'}}>
+            <h3>Welcome</h3>
+            <p>Check out these awesome features</p>
         </div>
     );
 };
